fc01442afd4a88ae2b8e8fdca3c922e62111f4b3
martabajo <martabg2492@gmail.com>
2015-01-12 09:50:17 +0100

    Metodo multiplesOfFive que imprime todos los multimos entre 10 y 95

---
+++ Test.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    public void multiplesOfFive()
+    {
+        int multiplos = 15;
+        while (multiplos < 95)
+        {
+            System.out.println(multiplos);
+            multiplos = multiplos + 5;
+        }
+    }
+
+  
+
+}
